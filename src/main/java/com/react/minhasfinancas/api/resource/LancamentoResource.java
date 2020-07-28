5ba03eb24657b3f6ab7f6ca279fd453e979442e1
Mauricio Machado <licio.machado@hotmail.com>
2020-07-28 01:44:26 -0300

    implementado controllers e funcionalidades necessarias

--- src/main/java/com/react/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/react/minhasfinancas/api/resource/LancamentoResource.java
@@ -3,6 +3,8 @@ package com.react.minhasfinancas.api.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.Entity;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.react.minhasfinancas.api.dto.AtualizaStatusDTO;
 import com.react.minhasfinancas.api.dto.LancamentoDTO;
 import com.react.minhasfinancas.exception.RegraNegocioException;
 import com.react.minhasfinancas.model.entity.Lancamento;
@@ -80,6 +83,27 @@ public class LancamentoResource {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 		}).orElseGet( () -> new ResponseEntity("Lancamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST) );
+	}
+	
+	@PutMapping("{id}/atualiza-status")
+	public ResponseEntity atualizarStatus( @PathVariable("id") Long id , @RequestBody AtualizaStatusDTO dto ) {
+		return service.obterPorId(id).map( entity -> {
+			StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+			
+			if(statusSelecionado == null) {
+				return ResponseEntity.badRequest().body("Não foi possível atualizar o status do lançamento, envie um status válido.");
+			}
+			
+			try {
+				entity.setStatus(statusSelecionado);
+				service.atualizar(entity);
+				return ResponseEntity.ok(entity);
+			}catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		
+		}).orElseGet( () ->
+		new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST) );
 	}
 	
 	@DeleteMapping("{id}")
--- src/main/java/com/react/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/react/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.react.minhasfinancas.exception.RegraNegocioException;
 import com.react.minhasfinancas.model.entity.Lancamento;
 import com.react.minhasfinancas.model.enums.StatusLancamento;
+import com.react.minhasfinancas.model.enums.TipoLancamento;
 import com.react.minhasfinancas.model.repository.LancamentoRepository;
 import com.react.minhasfinancas.service.LancamentoService;
 
@@ -96,6 +97,21 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Override
 	public Optional<Lancamento> obterPorId(Long id) {
 		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BigDecimal obterSaldoPorUsuario(Long id) {
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+		
+		if (receitas == null ) {
+			receitas = BigDecimal.ZERO;
+		}
+		if (despesas == null ) {
+			despesas = BigDecimal.ZERO;
+		}
+		return receitas.subtract(despesas);
 	}
 
 	
